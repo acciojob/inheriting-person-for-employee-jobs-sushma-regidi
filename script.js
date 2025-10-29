972a4a4fd564db787ae56ef8f21e8e8e752ebee9
sushma-regidi <regidisushma@gmail.com>
2025-10-29 18:02:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
-// complete this js code
-function Person(name, age) {}
+// Define the Person class
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
 
-function Employee(name, age, jobTitle) {}
+  greet() {
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+  }
+}
 
-// Do not change code below this line
+// Define the Employee class that inherits from Person
+class Employee extends Person {
+  constructor(name, age, jobTitle) {
+    super(name, age); // Call parent constructor
+    this.jobTitle = jobTitle;
+  }
+
+  jobGreet() {
+    console.log(
+      `Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`
+    );
+  }
+}
+
+// Export classes for Cypress testing
 window.Person = Person;
 window.Employee = Employee;
+
